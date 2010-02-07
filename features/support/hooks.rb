f375ca638bbf775837b447f1f5cae99d2abcc900
Jordan West, Westward Web Development <jordan.west@westwardwd.com>
2010-02-07 15:00:06 -0500

    left some nasty commented code laying around from last commit

--- features/support/hooks.rb
+++ features/support/hooks.rb
@@ -3,14 +3,10 @@ def demo_feature_files
 end
 
 After do
-  #demo_files_after = demo_feature_files
-  #to_remove = demo_files_after - @demo_files_before
   if @generated_demos
     @generated_demos.each do |file|
       #puts file
       `rm #{file}`
     end
   end
-  #{}`rm features/feature_tagged.feature` if File.exists?('features/feature_tagged.feature')
-  #{}`rm features/basic.feature` if File.exists?('features/basic.feature')
 end
